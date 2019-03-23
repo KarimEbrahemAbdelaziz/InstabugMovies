85451bcbc8630b96efbdb03c84343f3e45c989d3
KarimEbrahemAbdelaziz <karimabdelazizmansour@gmail.com>
2019-03-23 22:44:19 +0200

    Add Movie entitiy

---
+++ InstabugMovies/InstabugMovies/Core/Entities/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  InstabugMovies
+//
+//  Created by Karim Ebrahem on 3/23/19.
+//  Copyright © 2019 Karim Ebrahem. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var id: String
+    var title: String
+    var overview: String
+    var date: String
+    var poster: String
+}
